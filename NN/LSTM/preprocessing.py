d47a776842fb466046bfa696670040502f996ca3
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2025-06-05 20:33:30 +0200

    Remove unnecessary parts

--- NN/LSTM/preprocessing.py
+++ NN/LSTM/preprocessing.py
@@ -13,28 +13,3 @@ omega = np.gradient(th_train, dt)
 np.save("theta.npy", th_train)
 np.save("omega.npy", omega)
 np.save("u.npy", u_train)
-
-# def create_IO_data(u,y,na,nb):
-#     X = []
-#     Y = []
-#     for k in range(max(na,nb), len(y)):
-#         X.append(np.concatenate([u[k-nb:k],y[k-na:k]]))
-#         Y.append(y[k])
-#     return np.array(X), np.array(Y)
-
-# na = 11
-# nb = 10
-# X, Y = create_IO_data(u_train, th_train, na, nb)
-
-# # Split into train/val/test
-# Xtemp, Xtest_final, Ytemp, Ytest_final = train_test_split(X, Y, test_size=0.15, random_state=42)
-# Xtrain_final, Xval, Ytrain_final, Yval = train_test_split(Xtemp, Ytemp, test_size=0.1765, random_state=42)
-# Xtrain_final = np.expand_dims(Xtrain_final, axis=-1)
-# Xval = np.expand_dims(Xval, axis=-1)
-
-# np.save("Xtrain_final.npy", Xtrain_final)
-# np.save("Ytrain_final.npy", Ytrain_final)
-# np.save("Xval.npy", Xval)
-# np.save("Yval.npy", Yval)
-# np.save("Xtest_final.npy", Xtest_final)
-# np.save("Ytest_final.npy", Ytest_final)
